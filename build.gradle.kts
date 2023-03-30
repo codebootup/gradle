57692ee1a46621e0dbce4874c9e6e6320ec396b3
Lee Cooper <lee@codebootup.com>
2023-03-30 12:37:52 +0800

    #1 - Add initial kotlin build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,3 +8,8 @@ version = (project.properties["buildVersion"] ?: "1.0.0-SNAPSHOT")
 repositories{
     mavenCentral()
 }
+
+dependencies{
+    implementation("org.jetbrains.kotlin:kotlin-allopen:1.8.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+}
